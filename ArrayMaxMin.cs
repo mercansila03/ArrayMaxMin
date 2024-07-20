4618d42e96320359423734a178b8db651eef4df9
SılaMercan <120477952+mercansila03@users.noreply.github.com>
2024-07-20 11:37:36 +0300

    Add files via upload

---
+++ ArrayMaxMin.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dizi2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sayilar = new int[10];
+            int enbuyuk = int.MinValue;
+            int enkucuk = int.MaxValue;
+            int toplam = 0;
+
+            for (int i=0;i < sayilar.Length; i++)
+            {
+                Console.Write(i + 1 + ".sayıyı giriniz: ");
+                int girilen=Convert.ToInt32(Console.ReadLine());
+                toplam += girilen;
+                if (girilen >enbuyuk){
+
+                    enbuyuk = girilen ;
+                }
+                else if (girilen < enkucuk){
+
+                    enkucuk = girilen;
+                }                         
+                
+            }            
+            
+            Console.WriteLine("Dizinin en büyük elemanı: " +enbuyuk );
+            Console.WriteLine("Dizinin en küçük elemanı: " + enkucuk);
+            Console.WriteLine("Toplam: " + toplam);
+            Console.ReadKey();
+
+
+        }
+    }
+}
